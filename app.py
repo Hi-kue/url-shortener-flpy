ceb59308c360e41e1e8961418cafbd36a79ed245
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-05-22 13:02:04 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, redirect, render_template
+from flask_cors import CORS
+from gevent.pywsgi import WSGIServer
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/about', methods=['GET'])
+def about_section():
+    return 'This is a URL Shortener API'
+
+
+if __name__ == '__main__':
+    http_server = WSGIServer(('127.0.0.0', 5000), app)
+    http_server.serve_forever()
